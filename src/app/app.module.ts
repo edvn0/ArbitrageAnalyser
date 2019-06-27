c7458ed56d997b24c85edb19d624d0c0bd2900aa
Edwin Carlsson <edwin98carlsson@gmail.com>
2019-06-27 17:01:39 +0200

    First upload.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from '../components/navigation/navigation.component';
+import { SearchComponent } from '../components/search/search.component';
+import { ProductsComponent } from '../components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SearchComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
